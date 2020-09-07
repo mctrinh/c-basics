d7be7118c4e6530942b7707ec8840624b0cceffd
mctrinh <mctrinh@jbnu.ac.kr>
2020-09-07 16:35:52 +0900

    Do not see comments in hello.c in the last push?

--- Project1/hello.c
+++ Project1/hello.c
@@ -5,3 +5,8 @@ int main()
     printf("Hello, World!");
     return 0;
 }
+
+// #include is a preprocessor command that tells the compiler to include the contents of stdio.h (standard input and output) file in the program.
+// stdio.h file contains functions such as scanf() and printf() to take input and display output respectively.
+// the execution of a C program starts from the main() function.
+// return 0; statement is the "Exit status" of the program. In simple terms, the program ends with this statement.
